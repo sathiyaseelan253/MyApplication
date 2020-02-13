34b4759d3fc627c2962b259171c82d8cab5c8077
sathiyaseelan253 <sathyaseelan253@gmail.com>
2020-02-13 23:26:58 +0530

    Add project files.

---
+++ MyApplication/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Hi from default index action method";
+        }
+        public string AnotherIndex()
+        {
+            return "Hello from another index action method";
+        }
+    }
+}
